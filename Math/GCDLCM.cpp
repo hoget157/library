c3684e6b5a28710418cb255f80557c68308b4ec5
hoget157 <hogethirit157@gmail.com>
2020-02-26 04:18:00 +0900

    refactor gcd/lcm

--- Math/GCDLCM.cpp
+++ Math/GCDLCM.cpp
@@ -1,10 +1,3 @@
-template<class T>
-T gcd(T a,T b){
-	return b ? gcd(b,a % b) : a;
-}
-
-template<class T>
-T lcm(T a,T b){
-	return a / gcd(a,b) * b;
-}
+template<class T> T gcd(T a,T b){ return b ? gcd(b,a % b) : a; }
+template<class T> T lcm(T a,T b){ return a / gcd(a,b) * b; }
 //verified: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3835971
