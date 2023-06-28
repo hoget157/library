6f7db6a75477f4334650e5999af8694a731d9d89
hoget157 <hogethirit157@gmail.com>
2023-06-28 17:21:23 +0900

    modify rolling hash

--- String/RollingHash.cpp
+++ String/RollingHash.cpp
@@ -1,37 +1,46 @@
-const long long baseNum = 3,bases[] = {10007,10009,10037};
-
-template<long long mod = 1000000007>
+template<class T>
 struct RollingHash{
-	using ll = long long;
-	vector<ll> hashes[baseNum],powers[baseNum];
-	RollingHash(string &s){
-		int n = s.length();
-		for(int i = 0;i < baseNum;i++){
-			hashes[i].resize(n + 1,0);
-			powers[i].resize(n + 1,1);
-			for(int j = 0;j < n;j++){
-				powers[i][j + 1] = powers[i][j] * bases[i] % mod;
-				hashes[i][j + 1] = (hashes[i][j] * bases[i] + s[j]) % mod;
-			}
-		}
+	using ll = __int128_t;
+	const ll mod = (1ll << 61) - 1;
+	const ll bases[10] = {10007, 10009, 10037, 10039, 10061, 10067, 10069, 10079, 10091, 10093};
+	
+	int n;
+	ll base;
+	vector<ll> powers;
+	vector<vector<ll>> hashes;
+
+	inline ll calc_mod(const ll x) const{
+		ll t = (x >> 61) + (x & mod);
+		return (t >= mod ? t - mod : t);
 	}
-	ll getHash(int l,int r,int b){
-		return (hashes[b][r] - hashes[b][l] * powers[b][r - l] % mod + mod) % mod;
+
+	RollingHash(const int n) : n(n), powers(n + 1, 1){
+		mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
+		uniform_int_distribution<int> dist(0, 9);
+		base = bases[dist(mt)];
+		for(int i = 0; i < n; i++) powers[i + 1] = calc_mod(powers[i] * base);
 	}
-	vector<ll> getHash(int l = 0,int r = -1){
-		if(r == -1) r = hashes[0].size() - 1;
-		vector<ll> ret(baseNum);
-		for(int i = 0;i < baseNum;i++){
-			ret[i] = getHash(l,r,i);
-		}
-		return ret;
+
+	int add(const string &s){
+		vector<ll> a(s.length());
+		for(int i = 0; i < (int)s.length(); i++) a[i] = s[i];
+		return add(a);
 	}
-	static bool sameString(vector<ll> v1,vector<ll> v2){
-		assert(v1.size() == v2.size());
-		for(int i = 0;i < v1.size();i++){
-			if(v1[i] != v2[i]) return false;
-		}
-		return true;
+
+	int add(const vector<T> &a){
+		const int m = a.size(), x = hashes.size();
+		hashes.emplace_back(m + 1, 0);
+		for(int i = 0; i < m; i++) hashes[x][i + 1] = calc_mod(hashes[x][i] * base + a[i]);
+		return x;
+	}
+
+	inline ll hash(const int x, const int l = 0, int r = -1) const{
+		if(r == -1) r = hashes[x].size() - 1;
+		return calc_mod(hashes[x][r] - hashes[x][l] * powers[r - l] + mod * mod);
+	}
+	
+	inline bool is_same(const int x, const int lx, const int rx, const int y, const int ly, const int ry) const{
+		return hash(x, lx, rx) == hash(y, ly, ry);
 	}
 };
-//verified: http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3985050
+// https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=7984725#1
